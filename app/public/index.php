a312aa1077afc6758fc63958b819e6e8d7f885aa
pauldahacker <paul.demasca@gmail.com>
2026-04-24 18:07:51 +0200

    added nginx Dockerfile, views/ folder and PHP has access to postgreSQL databases using pdo (PHP Data Objects) interface.

--- app/public/index.php
+++ app/public/index.php
@@ -1,1 +1,22 @@
-<?php echo "Hello World"; ?>
+<?php
+require __DIR__ . '/../src/greet.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST["username"];
+    echo greet($name);
+}
+
+try {
+    $pdo = new PDO('pgsql:host=db;dbname=camagru', 'user', 'pass');
+}
+catch (PDOException $e) {
+    echo "Database connection failed: " . $e->getMessage();
+    exit;
+}
+
+$statement = $pdo->prepare("SELECT * FROM users");
+$statement->execute();
+var_dump($statement->fetchAll());
+
+require '../src/views/index.view.php';
+?>
---
+++ app/src/greet.php
@@ -0,0 +1,6 @@
+<?php
+
+function greet($name) {
+    return "Hello, " . htmlspecialchars($name) . "!";
+}
+?>
